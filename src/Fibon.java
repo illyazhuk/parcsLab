0fd752048175fc91875eb0d96e958d1414c143ad
illyazhuk <66369819+illyazhuk@users.noreply.github.com>
2020-06-05 01:41:24 +0300

    Update Fibon.java

--- src/Fibon.java
+++ src/Fibon.java
@@ -13,19 +13,15 @@ public class Fibon implements AM {
     public void run(AMInfo info) {
       long n;
       try {
-          /*byte[] buf = new byte[200];
-          System.out.print("Enter n: ");
-          System.in.read(buf);
-          n=new Long(new String(buf).trim()).longValue();*/
-
           BufferedReader in = new BufferedReader(new FileReader(info.curtask.findFile("Fibon.data")));
           n = new Long(in.readLine()).longValue();
-      } catch (IOException e) {e.printStackTrace(); return;}
+      } 
+        catch (IOException e) {e.printStackTrace(); return;}
 
       point p1 = info.createPoint();
       channel c1 = p1.createChannel();
       p1.execute("Fib");
-      c1.write(n);
+      c1.write(2);
 
       System.out.println("Waiting for result...");
       long r = c1.readLong();
