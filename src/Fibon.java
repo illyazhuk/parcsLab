bae53a6d3f35fdc4c175c7223e81742ac64abfa4
illyazhuk <66369819+illyazhuk@users.noreply.github.com>
2020-06-05 01:31:29 +0300

    Update Fibon.java

--- src/Fibon.java
+++ src/Fibon.java
@@ -30,7 +30,30 @@ public class Fibon implements AM {
       System.out.println("Waiting for result...");
       long r = c1.readLong();
       System.out.println("Result found.");
-    System.out.println("JUKKKKK");
+        int counter = 0;
+         boolean prime[] = new boolean[(int)n+1]; 
+        for(int i=0;i<n;i++) 
+            prime[i] = true; 
+          
+        for(int p = 2; p*p <=n; p++) 
+        { 
+            // If prime[p] is not changed, then it is a prime 
+            if(prime[p] == true) 
+            { 
+                // Update all multiples of p 
+                for(int i = p*2; i <= n; i += p) 
+                    prime[i] = false; 
+            } 
+        } 
+          
+        // Print all prime numbers 
+        for(int i = 2; i <= n; i++) 
+        { 
+            if(prime[i] == true) 
+                counter++;
+        } 
+    System.out.println(counter);
+        
       //System.out.println("F"+n+"="+r);
       //try{
         //  PrintWriter out = new PrintWriter(new FileWriter(info.curtask.addPath("Fibon.res")));
